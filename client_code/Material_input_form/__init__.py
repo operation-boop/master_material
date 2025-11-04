7fd763912a4ba5d8cfcd886f6857aa5e008c76ad
wymao906@gmail.com <wymao906@gmail.com>
2025-11-04 08:38:02 +0000

    Created Material_input_form; Edited Style_list, Material_detail and Material_list; Deleted Staff_list

--- client_code/Material_input_form/__init__.py
+++ client_code/Material_input_form/__init__.py
@@ -1,15 +1,15 @@
-from ._anvil_designer import Staff_listTemplate
+from ._anvil_designer import Material_input_formTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
 
 
-class Staff_list(Staff_listTemplate):
+class Material_input_form(Material_input_formTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
