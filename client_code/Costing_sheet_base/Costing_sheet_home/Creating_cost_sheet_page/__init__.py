3cd3cb145ac310bd3980a83c2fd8c83bba6436d3
athan2aquino@gmail.com <athan2aquino@gmail.com>
2025-11-05 14:40:41 +0000

    Edited Costing_sheet_base.Costing_sheet_home.Creating_cost_sheet_page

--- client_code/Costing_sheet_base/Costing_sheet_home/Creating_cost_sheet_page/__init__.py
+++ client_code/Costing_sheet_base/Costing_sheet_home/Creating_cost_sheet_page/__init__.py
@@ -27,20 +27,34 @@ class Creating_cost_sheet_page(Creating_cost_sheet_pageTemplate):
     self.card_costing_sheet_base.clear()
     self.card_costing_sheet_base.add_component(Costing_sheet_base())
 
+    self.parent.card_costing_sheet_base.clear() 
+    self.parent.card_costing_sheet_base.add_component(Costing_sheet_home())
+  
   def radio_button_basic_information_change(self, **event_args):
     """This method is called when this radio button is selected (but not deselected)"""
     self.column_panel_information.clear()
     self.column_panel_information.add_component(a_Basic_information_page())
 
   def radio_button_bill_of_material_change(self, **event_args):
-     """This method is called when this radio button is selected (but not deselected)"""
+    """This method is called when this radio button is selected (but not deselected)"""
     self.column_panel_information.clear()
     self.column_panel_information.add_component(b_Bom_page())
 
   def radio_button_processing_cost_change(self, **event_args):
-      """This method is called when this radio button is selected (but not deselected)"""
+    """This method is called when this radio button is selected (but not deselected)"""
     self.column_panel_information.clear()
-    self.column_panel_information.add_component(c_Processing_cost)
+    self.column_panel_information.add_component(c_Processing_cost())
+
+  def radio_button_overhead_cost_change(self, **event_args):
+    """This method is called when this radio button is selected (but not deselected)"""
+    self.column_panel_information.clear()
+    self.column_panel_information.add_component(d_Overhead_cost())
+
+  def radio_button_profit_scenario_change(self, **event_args):
+    """This method is called when this radio button is selected (but not deselected)"""
+    self.column_panel_information.clear()
+    self.column_panel_information.add_component(e_Profit_scenarios())
+  
 
     
 
