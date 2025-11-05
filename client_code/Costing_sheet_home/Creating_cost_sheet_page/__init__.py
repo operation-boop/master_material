ac33dfc34b7849a149201b1fa97b3c0f58264983
athan2aquino@gmail.com <athan2aquino@gmail.com>
2025-11-05 07:04:02 +0000

    Created Costing_sheet_home, Costing_sheet_home.Creating_cost_sheet_page, Costing_sheet_home.Creating_cost_sheet_page.a_Basic_information_page, Costing_sheet_home.Creating_cost_sheet_page.b_Bom_page and 4 more; Edited settings, Costing_sheet_base and Home; Deleted Costing_sheet_base.Creating_cost_sheet_page, Costing_sheet_base.Creating_cost_sheet_page.a_Basic_information_page, Costing_sheet_base.Creating_cost_sheet_page.b_Bom_page, Costing_sheet_base.Creating_cost_sheet_page.c_Processing_cost and 3 more

--- client_code/Costing_sheet_home/Creating_cost_sheet_page/__init__.py
+++ client_code/Costing_sheet_home/Creating_cost_sheet_page/__init__.py
@@ -9,9 +9,15 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
+
 class Creating_cost_sheet_page(Creating_cost_sheet_pageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_cancel_create_cost_sheet_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.card_costing_sheet_home.clear()
+    self.card_costing_sheet_home.add_component()
--- client_code/Costing_sheet_home/__init__.py
+++ client_code/Costing_sheet_home/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Costing_sheet_baseTemplate
+from ._anvil_designer import Costing_sheet_homeTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -7,17 +7,16 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..Costing_sheet_home import Costing_sheet_home
+from .Creating_cost_sheet_page import Creating_cost_sheet_page
 
-
-class Costing_sheet_base(Costing_sheet_baseTemplate):
+class Costing_sheet_home(Costing_sheet_homeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.card_costing_sheet_base.add_component(Costing_sheet_home())
+
     # Any code you write here will run before the form opens.
 
   def button_create_new_sheet_click(self, **event_args):
     """This method is called when the button is clicked"""
-    self.card_costing_sheet_base.clear()
-    self.card_costing_sheet_base.add_component()
+    self.card_costing_sheet_home.clear()
+    self.card_costing_sheet_home.add_component(Creating_cost_sheet_page())
