d6717d191896fa4fa8a9a0125d119883c16911ec
athan2aquino@gmail.com <athan2aquino@gmail.com>
2025-11-05 02:55:29 +0000

    Created Costing_sheet_base, Costing_sheet_base.Creating_cost_sheet_page, Costing_sheet_base.Creating_cost_sheet_page.Basic_information_page, Costing_sheet_base.Creating_cost_sheet_page.Bom_page and 3 more; Edited Home

---
+++ client_code/Costing_sheet_base/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import Costing_sheet_baseTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class Costing_sheet_base(Costing_sheet_baseTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
