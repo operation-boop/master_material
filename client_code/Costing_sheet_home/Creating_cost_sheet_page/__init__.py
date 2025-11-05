2306506f3bc053c9944396a28b56523f84c251da
athan2aquino@gmail.com <athan2aquino@gmail.com>
2025-11-05 07:31:53 +0000

    Edited Costing_sheet_base, Costing_sheet_home.Creating_cost_sheet_page and Home

--- client_code/Costing_sheet_home/Creating_cost_sheet_page/__init__.py
+++ client_code/Costing_sheet_home/Creating_cost_sheet_page/__init__.py
@@ -7,7 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+from ...Costing_s
 
 
 class Creating_cost_sheet_page(Creating_cost_sheet_pageTemplate):
@@ -19,5 +19,5 @@ class Creating_cost_sheet_page(Creating_cost_sheet_pageTemplate):
 
   def button_cancel_create_cost_sheet_click(self, **event_args):
     """This method is called when the button is clicked"""
-    self.card_costing_sheet_home.clear()
-    self.card_costing_sheet_home.add_component()
+    self.card_costing_sheet_base.clear()
+    self.card_costing_sheet_base.add_component(co
