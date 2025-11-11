81e700082d0452f7710acce1ead0feb2e7126f08
wymao906@gmail.com <wymao906@gmail.com>
2025-11-11 03:17:31 +0000

    Created Style_input_form.Color_tag_itemtemplate; Edited Style_input_form; Deleted Style_input_form.Color_itemtemplate

--- client_code/Style_input_form/Color_tag_itemtemplate/__init__.py
+++ client_code/Style_input_form/Color_tag_itemtemplate/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Color_itemtemplateTemplate
+from ._anvil_designer import Color_tag_itemtemplateTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,9 +9,13 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Color_itemtemplate(Color_itemtemplateTemplate):
+class Color_tag_itemtemplate(Color_tag_itemtemplateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def remove_btn_click(self, **event_args):
+    # Call back to parent form to remove the item
+    self.parent.raise_event('x-remove-color', value=self.item)
--- client_code/Style_input_form/__init__.py
+++ client_code/Style_input_form/__init__.py
@@ -14,4 +14,38 @@ class Style_input_form(Style_input_formTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+    self.color_list = []   # store colors temporarily
+    self.size_list = []    # store sizes
+    self.repeating_panel_colors.items = self.color_list
+    self.repeating_panel_sizes.items = self.size_list
+
+    # Listen for remove request
+    self.set_event_handler('x-remove-color', self.remove_color)
+    self.set_event_handler('x-remove-size', self.remove_size)
     # Any code you write here will run before the form opens.
+
+  # ---- COLOR ADD ----
+  def add_color_btn_click(self, **event_args):
+    color = (self.color.text or "").strip()
+    if color and color not in self.color_list:
+      self.color_list.append(color)
+      self.repeating_panel_colors.items = self.color_list[:]   # refresh
+    self.color.text = ""
+
+  def remove_color(self, value, **event_args):
+    if value in self.color_list:
+      self.color_list.remove(value)
+      self.repeating_panel_colors.items = self.color_list[:]
+
+  # ---- SIZE ADD ----
+  def add_size_btn_click(self, **event_args):
+    size = (self.size.text or "").strip()
+    if size and size not in self.size_list:
+      self.size_list.append(size)
+      self.repeating_panel_sizes.items = self.size_list[:]
+    self.size.text = ""
+
+  def remove_size(self, value, **event_args):
+    if value in self.size_list:
+      self.size_list.remove(value)
+      self.repeating_panel_sizes.items = self.size_list[:]
