3cea31d89fc9c951bf94f2eb0eb97560cded9a5d
athan2aquino@gmail.com <athan2aquino@gmail.com>
2025-11-11 08:48:45 +0000

    Edited bill_of_materials

--- server_code/bill_of_materials.py
+++ server_code/bill_of_materials.py
@@ -8,6 +8,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 from datetime import datetime
+from . import utilities
 
 # ============================================
 # BILL OF MATERIAL (BOM) - CRUD Operations
