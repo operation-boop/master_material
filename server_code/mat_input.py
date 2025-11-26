b928ccb936a4613fc0564156468cdc477f13a130
ggjyx4@gmail.com <ggjyx4@gmail.com>
2025-11-26 07:05:30 +0000

    Edited mat_input

--- server_code/mat_input.py
+++ server_code/mat_input.py
@@ -393,3 +393,4 @@ def validate_required_fields(document_id):
 
   is_valid, missing_fields = _validate_required_fields(current_version)
   return {"is_valid": is_valid, "missing_fields": missing_fields}
+
